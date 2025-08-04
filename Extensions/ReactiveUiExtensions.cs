edd05a1f9bc4ae4973defee95a588a02f78105bc
Dimitiry <weerci@yandex.ru>
2025-08-04 12:23:54 +0700

    +++

---
+++ Extensions/ReactiveUiExtensions.cs
@@ -0,0 +1,16 @@
+﻿using DynamicData;
+using ReactiveUI;
+using System.Reactive.Linq;
+using System.Windows.Input;
+
+namespace Func.Extensions;
+
+public static class ReactiveUiExtensions
+{
+    public static void ExecuteIfPossible<TParam, T, TResult>(this ReactiveCommand<TParam, TResult> cmd, T t)
+    {
+        if (cmd is ICommand c && c.CanExecute(t))
+            c.Execute(t);
+    }
+
+}
